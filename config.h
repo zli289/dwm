27a44dc6a74c93aa6bcae9ba18ab53d0c40c50a0
Zequn <zli289@gwu.edu>
2020-07-02 03:08:59 -0400

    status bar control

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 5;       /* vert outer gap between windows
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=10" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=10";
+static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=12" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -71,13 +71,22 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "google-chrome-stable", NULL };
+static const char *termcmd[]       = { "st", NULL };
+static const char *browsercmd[]    = { "google-chrome-stable", NULL };
+static const char *backlight_inc[] = {"/home/li/scripts/backlight-inc.sh", NULL};
+static const char *backlight_dec[] = {"/home/li/scripts/backlight-dec.sh", NULL};
+static const char *vol_up[]        = {"/home/li/scripts/vol-up.sh", NULL};
+static const char *vol_down[]      = {"/home/li/scripts/vol-down.sh", NULL};
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,			XK_F2,	   spawn,	   {.v =browsercmd } },
+	{ MODKEY,			XK_e,	   spawn,	   {.v = browsercmd } },
+	{ MODKEY,			XK_F11,	   spawn,	   {.v = backlight_dec} },
+	{ MODKEY,			XK_F12,	   spawn,	   {.v = backlight_inc} },
+	{ MODKEY,			XK_F2,	   spawn,	   {.v = vol_down} },
+	{ MODKEY,			XK_F3,     spawn,	   {.v = vol_up} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
